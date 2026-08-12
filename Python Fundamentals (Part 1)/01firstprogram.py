359e6acdf1c1a3708e3dcf47e418d67dfb849346
Pari Sangamnerkar <parisangamnerkar@gmail.com>
2026-08-12 23:44:40 +0530

    Add Python fundamentals first program

---
+++ Python Fundamentals (Part 1)/01firstprogram.py
@@ -0,0 +1,5 @@
+print("hello world")
+print("with python") #different line
+print("hello world", "with python") #same line
+print("hello \n world", "with python") #\n for next line
+print("hello \nworld", "with python")
